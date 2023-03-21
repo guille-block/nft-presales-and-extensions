75a60961ee428e0a2c12dbb5f9a15a38d8f17242
GuillermoFairbairn <fairbairnguillermo@gmail.com>
2023-03-21 10:13:03 -0300

    {NEW} Enumerable contracts

---
+++ src/enumerable/EnumerableNFT.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+/**
+ * @title EnumerableNFT
+ * @dev This contract allows users to mint non-fungible tokens (NFTs) in a sequential and enumerable manner.
+ */
+contract EnumerableNFT is ERC721Enumerable {
+    uint256 constant MAX_SUPPLY = 20;
+
+    /**
+     * @notice Initialize the ERC721Enumerable contract.
+     * @param name_ The name of the NFT token.
+     * @param symbol_ The symbol of the NFT token.
+     */
+    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {}
+
+    /**
+     * @notice Mint a new NFT to the caller with 1 as the starting id.
+     */
+    function mint() public {
+        uint id = totalSupply() + 1;
+        require(id <= MAX_SUPPLY, "MAXED AMOUNT REACHED");
+        _safeMint(msg.sender, id);
+    }
+}
---
+++ src/enumerable/PrimeNFTCounter.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+/**
+ * @title PrimeNFTCounter
+ * @dev A contract for counting the number of prime numbered ERC721 tokens owned by a particular address.
+ */
+contract PrimeNFTCounter {
+    ERC721Enumerable nft;
+    uint256[] specialIds = [2, 3, 5, 7, 11, 13, 17, 19];
+
+    /**
+     * @dev Initializes the contract instance with the address of an existing ERC721Enumerable contract.
+     * @param _nft The address of the ERC721Enumerable contract.
+     */
+    constructor(address _nft) {
+        nft = ERC721Enumerable(_nft);
+    }
+
+    /**
+     * @notice the number of prime numbered tokens owned by a particular address.
+     * @param user The address of the user for whom the count of prime numbered tokens is to be returned.
+     * @return primeCount The number of prime numbered tokens owned by the specified address.
+     */
+    function getPrimeCount(address user) public returns (uint256) {
+        uint userAmount = nft.balanceOf(user);
+        uint256 primeCount;
+        for (uint256 i = 0; i < userAmount; i++) {
+            uint userId = nft.tokenOfOwnerByIndex(user, i);
+            for (uint256 j = 0; j < specialIds.length; j++) {
+                if (specialIds[j] == userId) {
+                    primeCount++;
+                }
+            }
+        }
+        return primeCount;
+    }
+}
